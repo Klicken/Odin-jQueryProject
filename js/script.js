036999c3c2a77e12edd81bb6a10e3842a642b9f2
Martin Karlsson <klickas@live.se>
2016-01-02 17:28:58 +0100

    -a

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
-    for (var i = 0; i < 16; i++) {
-        $("body").append("<div>" + i + "</div>");
-        console.log(i);
+    for (var i = 1; i < 17; i++) {
+        for (var x = 1; x < 17; x++) {
+            $("#main").append("<div class='box' ></div>");
+        }
     }
+    
+    $("#clear").click(function () {
+        var width = prompt("Choose width");
+        var height = prompt("Choose height");
+        $("#main").empty();
+        for (var i = 0; i < width; i++) {
+            for (var x = 0; x < height; x++) {
+                $("#main").append("<div class='box'></div>");
+            }         
+        }
+    });
+});
+
+$(document).ready(function () {
+    $(".box").mouseenter(function () {
+            $(this).css("background-color", "white");
+    });
     
 });
